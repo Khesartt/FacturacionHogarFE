b6b7b15a43c3675be9d83d32e06c3339154c78f9
cesar reyes <cesarlu-12@hotmail.com>
2024-07-24 19:37:44 -0500

    fix minor problems

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl:"http://localhost:60060"
+  baseUrl:"http://localhost:60060",
+  getClientsUrl: "api/Client/getAll",
+  addClientUrl: "api/Client/Add",
+  lastLeaseReceipt: "api/LeaseReceipt",
 };
